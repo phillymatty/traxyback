a38ccbe55f0f6d4989d7b6c24ec31263a7c4d176
Eric Jones <ejones@securitytrax.com>
2021-03-25 17:07:11 -0600

    added overall rating star input if result query param present.

--- components/inputs/textarea.js
+++ components/inputs/textarea.js
@@ -6,10 +6,10 @@ const Textarea = props => (
    <div className="textArea">
 
        <TextareaAutosize
-       aria-label="minimum height" rowsMin={6}
-       defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-           ut labore et dolore magna aliqua."
-     />
+        aria-label="minimum height" rowsMin={6}
+        defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua."
+      />
    </div>
 )
 
