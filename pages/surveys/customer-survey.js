c3dd8be2537cf2fa84d0056f9d8387a353f747a2
Matt Smith <matthewjaredsmith@gmail.com>
2021-03-24 22:19:56 -0600

    fix defaults

--- pages/surveys/customer-survey.js
+++ pages/surveys/customer-survey.js
@@ -18,9 +18,9 @@ export async function getStaticProps() {
 export default function CustomerSurvey({ surveyData }) {
   const {query} = useRouter();
   const customerName = (surveyData.customer_name ? surveyData.customer_name : 'Valued Customer');
-  const companyName = (surveyData.company_name != '' ? surveyData.company_name : 'Sandbox Security');
-  const surveyType = (surveyData.survey_type != '' ? surveyData.survey_type : 'Security System Install');
-  const userName = (surveyData.user_name != '' ? surveyData.user_name : 'Michel Scott');
+  const companyName = (surveyData.company_name  ? surveyData.company_name : 'Sandbox Security');
+  const surveyType = (surveyData.survey_type ? surveyData.survey_type : 'Security System Install');
+  const userName = (surveyData.user_name ? surveyData.user_name : 'Michel Scott');
 
     return (
       <Layout>
