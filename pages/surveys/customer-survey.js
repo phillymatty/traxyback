4cd1783303888185b37820a6537ceed6c08dc18e
Matt Smith <matthewjaredsmith@gmail.com>
2021-03-24 23:25:34 -0600

    FIX LOAD

--- pages/surveys/customer-survey.js
+++ pages/surveys/customer-survey.js
@@ -10,9 +10,10 @@ import Input from '../../components/inputs/input'
 export async function getServerSideProps(context) {
     const query = context.query
     let surveyData
-    if (query.survey_id && query.survey_hash) {
+    if (query.survey_id) {
       const res = await fetch(`https://survey-hackathon-api.review.securitytrax.com/sandbox/user/v1/surveys/${query.survey_id}/actions/get_survey_data?survey_hash=${query.survey_hash}`)
       surveyData = await res.json()
+      console.warn('survey data', surveyData)
       if (!surveyData.questions) {
         surveyData = getSurveyQuestions()
       }
