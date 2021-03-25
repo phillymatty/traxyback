ca29c54b7c5925751c7c71c014835e3007075616
Matt Smith <matthewjaredsmith@gmail.com>
2021-03-24 22:58:23 -0600

    make everything dynamic

--- pages/surveys/custom-survey.js
+++ pages/surveys/custom-survey.js
@@ -8,11 +8,11 @@ import Input from '../../components/inputs/input'
 export async function getServerSideProps(context) {
   const query = context.query
   let surveyData
-  if (query.survey_id) {
-    const res = await fetch(`https://survey-hackathon-api.review.securitytrax.com/sandbox/user/v1/surveys/${query.survey_id}/actions/get_survey_data?survey_hash=1a2dd-22ac29cecf5b6f2f86f7fa8c4443e534`)
+  if (query.survey_id && query.survey_hash) {
+    const res = await fetch(`https://survey-hackathon-api.review.securitytrax.com/sandbox/user/v1/surveys/${query.survey_id}/actions/get_survey_data?survey_hash=${query.survey_hash}`)
     surveyData = await res.json()
     if (!surveyData.questions) {
-      surveyData = getCompanySurveyQuestions(query.survey_id)
+      surveyData = getSurveyQuestions()
     }
   } else {
     surveyData = getSurveyQuestions()
