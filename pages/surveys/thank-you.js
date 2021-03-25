1d6bf491dd40dc44801deb1627740e11ca297ec0
Eric Jones <ejones@securitytrax.com>
2021-03-25 15:15:40 -0600

    nice thank you page

--- pages/surveys/thank-you.js
+++ pages/surveys/thank-you.js
@@ -1,3 +1,24 @@
+import Layout from '../../components/layout'
+import Head from 'next/head'
+import Card from '../../components/cards/card'
+import CardCell from '../../components/cards/card-cell'
+
 export default function ThankYou() {
-    return <div>Thank You!</div>
+    return (
+        <Layout>
+        <Head>
+            <title>Success!</title>
+        </Head>
+        <Card>
+            <CardCell>
+                <picture>
+                    <source srcSet="/darkMode-sandbox.svg" media="(prefers-color-scheme: dark)" />
+                    <source srcSet="/sandbox.svg" media="(prefers-color-scheme: light), (prefers-color-scheme: no-preference)" />
+                    <img id="logo" src="/sandbox.svg" />
+                </picture>
+                <h1>Thank You</h1>
+            </CardCell>
+        </Card>
+        </Layout>
+    )
 }
