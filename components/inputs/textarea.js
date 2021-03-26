d8cb0ec42ea5c3ed87bc0c5c8e60ac87e40ebd86
Matt Smith <matthewjaredsmith@gmail.com>
2021-03-25 20:42:06 -0600

    text area working

--- components/inputs/textarea.js
+++ components/inputs/textarea.js
@@ -3,18 +3,26 @@ import React from 'react';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
 export default function Textarea({props}) {
+    const [newVal, setValue] = React.useState(props.question.answer);
 
+   const handleChange = (event) => {
+        setValue(event.target.value);
+
+        console.warn('event.target.innerHTML', event)
+        props.question.answer = newVal
+        
+        props.callback(props.surveyData, props.surveyHash)
+        console.warn(' props.question.answer',  props.question.answer)
+      }
 return (
    <div className="textArea">
 
-       <TextareaAutosize
-       onChange={(event) => {
-        props.question.answer = event.target.innerHTML.toString()
-        props.callback(props.surveyData, props.surveyHash)
-      }}
-       aria-label="minimum height" rowsMin={6}
-       defaultValue={props.question.answer}     />
-   </div>
+       <textarea
+       onChange={handleChange}
+        value={newVal}
+        rows='6'
+      />
+    </div>
 )
 
 }
